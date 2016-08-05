6ec3a91c593c7323a2a125f02144261a0dcd656b
Cabub <king_cabub@hotmail.com>
2016-08-05 12:08:13 -0500

    Add files via upload

---
+++ SeachFileNamesInZip/SeachFileNamesInZip/Module1.vb
@@ -0,0 +1,21 @@
+﻿Imports System.IO
+Imports System.IO.Compression
+Imports System.Text.RegularExpressions
+
+Module Module1
+
+    Sub Main(ByVal Args() As String)
+        Try
+            Dim RegX As New Regex(Args(0), RegexOptions.IgnoreCase)
+            Dim arch As New ZipArchive(File.Open(Args(1), FileMode.Open))
+            For Each f As ZipArchiveEntry In arch.Entries
+                If RegX.IsMatch(f.Name) Then
+                    Console.WriteLine(Args(1) & "\" & f.FullName)
+                End If
+            Next
+        Catch ex As Exception
+            Console.Error.WriteLine("ERROR: " & ex.Message & vbNewLine & ex.StackTrace)
+        End Try
+    End Sub
+
+End Module
---
+++ SeachFileNamesInZip/SearchFileNamesInZips/Module1.vb
@@ -0,0 +1,30 @@
+﻿Imports System.IO
+Imports System.IO.Compression
+Imports System.Text.RegularExpressions
+
+Module Module1
+
+    Sub Main(ByVal Args() As String)
+        Try
+            Dim RegX As New Regex(Args(0), RegexOptions.IgnoreCase)
+            If Not Args(1).EndsWith("\") Then
+                Args(1) = Args(1) & "\"
+            End If
+            If Directory.Exists(Args(1)) Then
+                For Each filename As String In Directory.GetFiles(Args(1))
+                    If filename.EndsWith(".zip") Then
+                        Dim arch As New ZipArchive(File.Open(filename, FileMode.Open))
+                        For Each f As ZipArchiveEntry In arch.Entries
+                            If RegX.IsMatch(f.Name) Then
+                                Console.WriteLine(Args(1) & filename & "\" & f.FullName)
+                            End If
+                        Next
+                    End If
+                Next
+            End If
+        Catch ex As Exception
+            Console.Error.WriteLine("ERROR: " & ex.Message & vbNewLine & ex.StackTrace)
+        End Try
+    End Sub
+
+End Module
